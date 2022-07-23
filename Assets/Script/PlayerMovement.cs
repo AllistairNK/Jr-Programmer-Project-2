bb81e7ed24b06abd2c5bc2072fed4d8c88b6421c
Allistair-unimas <64618@siswa.unimas.my>
2022-07-24 06:08:48 +0800

    Dog anims

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,10 +9,12 @@ public class PlayerMovement : MonoBehaviour
     private float turnSpeed = 120.0f;
     private float horizontalInput;
     private float forwardInput;
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,5 +26,23 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * horsePower * forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.Play("Base Layer.Bark",0,0);
+        }
+    }
+    public void PlayBark()
+    {
+        playerAnim.Play("Base Layer.Bark", 0, 0);
+    }
+    public void PlayEat()
+    {
+        playerAnim.Play("Base Layer.Eat", 0, 0);
+    }
+
+    public void PlaySit()
+    {
+        playerAnim.Play("Base Layer.SideDown", 0, 0);
     }
 }
